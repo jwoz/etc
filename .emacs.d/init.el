376ef5123f9608e4a077965449972b4c6e91c8b0
Juergen <p45052000@yahoo.com>
2021-01-09 20:18:56 -0500

    company with ycmd

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,3 +1,9 @@
+;; general settings
+(setq tab-width 2)
+(setq-default fill-column 120)
+(setq-default show-trailing-whitespace t)
+
+;; packages
 (require 'package)
 (add-to-list 'package-archives
          '("melpa" . "http://melpa.org/packages/") t)
@@ -17,9 +23,27 @@
 (require 'company)
 (add-hook 'after-init-hook 'global-company-mode)
 
-(setq company-backends (delete 'company-semantic company-backends))
+;; ycmd
+;; https://github.com/abingham/emacs-ycmd
+;; https://github.com/ycm-core/ycmd#building
+(require 'ycmd)
+(add-hook 'after-init-hook #'global-ycmd-mode)
+(add-hook 'c++-mode-hook 'ycmd-mode)
+(set-variable 'ycmd-server-command '("python" "/home/juergen/github/ycmd/ycmd"))
+
+(require 'company-ycmd)
+(company-ycmd-setup)
+
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; https://stackoverflow.com/questions/34117163/how-to-have-company-mode-irony-backend-and-irony-header-work-in-emacs
+;; for irony....
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;(setq company-backends (delete 'company-semantic company-backends))
 ;; (define-key c-mode-map  [(tab)] 'company-complete)
-;; (define-key c++-mode-map  [(tab)] 'company-complete)
+;;(define-key c++-mode-map  [(tab)] 'company-complete)
 
 ;; (add-to-list 'company-backends 'company-c-headers)
 ;; (add-to-list 'company-c-headers-path-system "/usr/include/c++/4.8/")
@@ -32,18 +56,19 @@
 
 
 ;; projectile
-(projectile-mode +1)
-(define-key projectile-mode-map (kbd "s-p") 'projectile-command-map)
-(define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
+;;(projectile-mode +1)
+;;(define-key projectile-mode-map (kbd "s-p") 'projectile-command-map)
+;;(define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
 
 
-(add-to-list 'load-path "~/.emacs.d/custom")
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (company projectile use-package))))
+ '(package-selected-packages
+   (quote
+    (company-c-headers company-irony ggtags use-package company))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
